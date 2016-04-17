97312d5947c7c4b6736a909a618a3f3809c96835
Karen Freeman-Smith <karenfreemansmith@gmail.com>
2016-04-16 17:37:48 -0700

    work per spec Hello olleH is a palindrome

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
 //alert("jQuery is working on " + $("h1").text()); //test for jQuery linked and loaded correctly
+var message = "";
+
+function palindrome(palindrome) {
+  var halfSize=parseInt(palindrome.length/2)
+  var firstHalf=palindrome.substring(0,halfSize);
+  var secondHalf=palindrome.substring(palindrome.length-halfSize, palindrome.length).split("");
+  secondHalf.reverse();
+  secondHalf=secondHalf.join("");
+
+  if(firstHalf===secondHalf) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+$("#testPhrase").submit(function(event) {
+  event.preventDefault();
+  if(palindrome($("#myPalindrome").val())) {
+    message = "Yes, you have a palindrome!";
+  } else {
+    message = "Oops! That's not a palindrome, try again.";
+  }
+
+  $("#results").text(message);
+});
