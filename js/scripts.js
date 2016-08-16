b87edd05447ff33773525fee62a0325ccc8ed616
Karen Freeman-Smith and Katy Henning <katy@>
2016-08-16 15:30:41 -0700

    fixed punctuation and space issues

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    $("h1").hide();
     var pp = $("#word").val().toUpperCase(); //pp=potential palendrome
     if(!palindrome(pp)) {
       var result= "No, it is not a palindome";
     } else {
       var result= "Yes, it is a palindrome";
     }
-    $("h1").text(result);
+    $("h1").text(result).fadeIn();
   });
 });
 
@@ -15,8 +16,16 @@ $(document).ready(function(){
 
 var palindrome = function(pp){
   var array = pp.split("");
-  var array2 = array.reverse();
+  var array2 = pp.split("");
+  for(var i=0; i<array.length; i++) {
+    if(!"ABCDEFGHIJKLMNOPQRSTUVWXYZ".includes(array[i])){
+      array[i]="";
+      array2[i]="";
+    }
+  }
+  array.reverse();
   var pp2 = array2.join("");
+  pp = array.join("");
   if (pp2 === pp){
     return true;
   }
